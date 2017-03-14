510c9206033a165410f865c540964fb3d9f4324f
Joshua Sierles <joshua@diluvia.net>
2017-03-14 16:49:39 +0530

    initial commit

---
+++ dependencies.R
@@ -0,0 +1,11 @@
+#!/usr/bin/env Rscript
+
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("readr")
+install.packages("leaflet")
+install.packages("lubridate")
+install.packages("maps")
+install.packages("ggmap")
+install.packages("ggthemes")
+install.packages("DT")
