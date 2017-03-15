b5a9d0db5d2ab08c1902ca71a0d52ab8cc336932
Joshua Sierles <joshua@diluvia.net>
2017-03-15 11:43:17 +0530

    install readxl from github for Alpine fix

--- dependencies.R
+++ dependencies.R
@@ -6,8 +6,13 @@ ipak <- function(pkg){
         install.packages(new.pkg, dependencies = TRUE)
     sapply(pkg, require, character.only = TRUE)
 }
-# usage
+
+# install latest readxl to fix https://github.com/tidyverse/readxl/issues/59
+install_github("tidyverse/readxl")
+
 packages <- c("ggplot2", "dplyr", "lubridate", "RColorBrewer", "scales", "gridExtra",
               "GGally", "gganimate", "plotly", "caret", "stringr", "DT", "data.table",
               "datatable", "maps", "ggmap", "ggthemes", "ggplot2", "leaflet", "readr")
+
+
 ipak(packages)
